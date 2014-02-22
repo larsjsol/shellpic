a7760a7f45458c9989b725ba1d14ce13435ee071
Lars Jørgen Solberg <supersolberg@gmail.com>
2014-02-22 22:35:46 +0100

    fix silly bug which would cause a crash if the image is smaller than the console

--- shellpic/__init__.py
+++ shellpic/__init__.py
@@ -11,6 +11,9 @@ def scale(image, width, height):
     """ scale an image while keeping the aspect ratio"""
     imgwidth, imgheight = image.size
 
+    scalewidth = 1
+    scaleheight = 1
+
     if imgwidth > width:
         scalewidth = float(width) / (imgwidth + 2)
     if imgheight > height * 2:
