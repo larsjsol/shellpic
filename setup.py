54e2c09e35d47ec006d554bf78018c0dc42cd2e8
Lars Jørgen Solberg <supersolberg@gmail.com>
2014-04-06 17:05:53 +0200

    ditch PIL and depend on Pillow instead

--- setup.py
+++ setup.py
@@ -8,15 +8,6 @@
 from distutils.core import setup
 import pkg_resources
 
-# Pillow's first version 1.0 was released on 2010-07-31, it's after PIL 1.1.7
-# (2009-11-15), so no need to restrain the version unless it's too new in the
-# future.
-imaging_requires = 'Pillow'
-try:
-    pkg_resources.get_distribution("Pillow")
-except pkg_resources.DistributionNotFound:
-    imaging_requires = 'PIL >= 1.1.7'
-
 setup(
     name='Shellpic',
     version='1.2.2',
@@ -29,7 +20,7 @@ setup(
     description='Display images using escape codes',
     long_description=open('README.rst').read(),
     install_requires=[
-        imaging_requires,
+        "Pillow",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
